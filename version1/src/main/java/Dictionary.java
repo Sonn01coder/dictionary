a2feb168340e5ade4ce16c74ee938b91c5bb28ed
son nguyen <trumle2k1@gmail.com>
2023-08-11 21:53:33 +0700

    complete version 1

---
+++ version1/src/main/java/Dictionary.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dictionary {
+    private List<Word> words;
+
+    public Dictionary(List<Word> words) {
+        this.words = words;
+    }
+    public Dictionary() {
+        this.words =new ArrayList<>();
+    }
+
+    public List<Word> getWords() {
+        return words;
+    }
+
+    public void setWords(List<Word> words) {
+        this.words = words;
+    }
+}
---
+++ version1/src/main/java/MainApplication.java
@@ -0,0 +1,7 @@
+public class MainApplication{
+    public static void main(String [] args) {
+        DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
+        dictionaryCommandline.dictionaryBasic();
+    }
+}
+
